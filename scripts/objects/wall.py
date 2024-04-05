79f2bee625585d6f02b815f1f7af49b0ff4a7035
zwiq123 <juluzwir@gmail.com>
2024-04-05 21:12:50 +0200

    changed wall texture

--- scripts/objects/wall.py
+++ scripts/objects/wall.py
@@ -1,15 +1,17 @@
 from .gameObject import GameObject
+import pygame
 
 
 class Wall(GameObject):
     def __init__(self, x, y, width, height, game):
-        super().__init__(x, y, width, height, None, game)
+        super().__init__(x, y, width, height, "textures/cobble.png", game)
         self.isBouncy = False
         super().set_color((128, 128, 128))
 
     def set_bouncy(self):
         self.isBouncy = True
         super().set_color((0, 128, 0))
+        self.img = pygame.transform.scale(pygame.image.load("textures/slime_block.png"), (self.width, self.height))
 
     def draw(self, offset):
         super().draw(offset)
